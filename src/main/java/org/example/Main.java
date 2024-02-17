6d4dfdbd22c3896cd778b902cdf5d90f06c92ea6
fridaeng <frida.engblom@abo.fi>
2024-02-17 23:31:27 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Prospect n: CustomerName wants to borrow X € for a period of Z years and pay E € each month");
+    }
+}
